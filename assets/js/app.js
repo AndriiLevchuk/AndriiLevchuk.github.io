7d4e6207c186bff854dc3f2462efe1a393886107
Andrew Levchuk <andriilevchukdev@gmail.com>
2018-01-29 10:59:01 +0200

    mobile +

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,142 +1,148 @@
 
-var VHChromeFix = function(selectors) {
-    var self = this;
-    var userAgent = navigator.userAgent.toLowerCase();
-    var isAndroidChrome = /chrome/.test(userAgent) && /android/.test(userAgent);
-    var isIOSChrome = /crios/.test(userAgent);
+var start = moment().subtract(29, 'days');
+    var end = moment();
+    $('#config-demo').daterangepicker({
+        "linkedCalendars": false,
+        "startDate": "01/09/2018",
+        "endDate": "01/15/2018"
+    }, function(start, end, label) {
+        console.log("New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')");
+    });
 
-    if (isAndroidChrome || isIOSChrome) {
-        // If we detected Chrome on Android or iOS
-        // Cache elements and trigger fix on init
-        this.getElements(selectors);
-        this.fixAll();
 
-        // Cache window dimensions
-        this.windowWidth = window.innerWidth;
-        this.windowHeight = window.innerHeight;
+    $(".applyBtn ").click(function () {
+        var start = $("input[name=daterangepicker_start]").val();
+        var end = $("input[name=daterangepicker_end]").val();
 
-        window.addEventListener('resize', function() {
-            // Both width and height changed (orientation change)
-            // This is a hack, as Android when eyboard pops up
-            // Triggers orientation change
-            if (self.windowWidth !== window.innerWidth && self.windowHeight !== window.innerHeight) {
-                self.windowWidth = window.innerWidth;
-                self.windowHeight = window.innerHeight;
-                self.fixAll();
-            }
-        });
+       $("#config-demo").empty();
+        $("#config-demo").append(start + ' - ' + end);
+
+    } );
+    // $("#config-demo").click(function () {
+    //     $("input[name=daterangepicker_start]").focus();
+    // });
+    if($("input[name=daterangepicker_start]").focus()){
+        $(".calendar.left .calendar-table").css("display","block");
+        $(".calendar.right .calendar-table").css("display","none");
+    }else if($("input[name=daterangepicker_end]").focus()){
+        $(".calendar.left .calendar-table").css("display","none");
+        $(".calendar.right .calendar-table").css("display","block");
     }
-};
+    // $("#config-demo").hover(function () {
+    //     $(".daterangepicker.dropdown-menu").css("display","block");
+    // });
 
-VHChromeFix.prototype.getElements = function(selectors) {
-    this.elements = [];
-    // Convert selectors to array if they are not
-    selectors = this.isArray(selectors) ? selectors : [selectors];
 
-    for (var i = 0; i < selectors.length; i++) {
-        // Get all elements for selector
-        var selector = selectors[i].selector;
-        var elements = document.querySelectorAll(selector);
+    // $('ul.menu-nav li.dropdown').hover(function() {
+    //     $(this).find('ul li.dropdown:nth-child(2) ').stop(true, true).delay(200).fadeIn(500);
+    // }, function() {
+    //     $(this).find('ul li.dropdown:nth-child(2)').stop(true, true).delay(200).fadeOut(500);
+    // });
+    //
+    // //$(".dropdown-menu.open [role=combobox]").append("<div></div>");
+    //
+    // $(document).ready(function(){
+    //     $('.dropdown-submenu a.test').on("click", function(e){
+    //         $(this).next('ul').toggle();
+    //         e.stopPropagation();
+    //         e.preventDefault();
+    //     });
+    // });
+    // $(function() {
+    //
+    //     var start = moment().subtract(29, 'days');
+    //     var end = moment();
+    //
+    //     function cb(start, end) {
+    //         $('#config-demo ').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    //     }
+    //
+    //     $('#config-demo').daterangepicker({
+    //         startDate: start,
+    //         endDate: end,
+    //
+    //     }, cb);
+    //
+    //     cb(start, end);
+    //
+    // });
+    $(document).ready(function() {
 
-        // Go through all elements for one selector to filter them
-        for (var j = 0; j < elements.length; j++) {
-            this.elements.push({
-                domElement: elements[j],
-                vh: selectors[i].vh
-            });
-        }
-    }
-};
-
-VHChromeFix.prototype.isArray = function(array) {
-    return Object.prototype.toString.call(array) === '[object Array]';
-};
-
-VHChromeFix.prototype.fixAll = function() {
-    for (var i = 0; i < this.elements.length; i++) {
-        var element = this.elements[i];
-        element.domElement.style.height = (window.innerHeight * element.vh / 100) + 'px';
-    }
-};
-$("a.next").click (function() {
-    $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
-    return false;
-});
-$(document).ready(function() {
-
-    var options = [
-        {
-            selector: '.hero',
-            vh: 100,
-        }
-    ];
-
-    var vhFix = new VHChromeFix(options);
-if($( window ).width()>1024) {
-    $.scrollify({
-        section: ".panel",
-       //easing: "easeOutExpo",
-        scrollbars: true,
-        scrollSpeed: 1200,
-        updateHash: true,
-        overflowScroll: true,
-        interstitialSection : ".hero, .footer",
-        before: function (i, panels) {
-            var ref = panels[i].attr("data-section-name");
-            $(".pagination .active").removeClass("active");
-            $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
-        },
-        after: function (i, panels) {
-
-        },
-        afterRender: function () {
-            var pagination = "<ul class=\"pagination\">";
-            var activeClass = "";
-            $(".panel").each(function (i) {
-                activeClass = "";
-                if (i === 0) {
-                    activeClass = "active";
+        $('.login.owl-carousel').owlCarousel({
+            loop:false,
+            margin:10,
+            merge:true,
+            navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:1,
+                    nav:true
+                },
+                600:{
+                    items:1,
+                    nav:false
+                },
+                1000:{
+                    items:1,
+                    nav:true,
+                    loop:false
                 }
-                pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") +"\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
-            });
-            pagination += "</ul>";
-            $(".menu").append(pagination);
-            $(".pagination a").on("click", $.scrollify.move);
-        }
-    });
-}else{
-    $.scrollify.disable();
-};
-    // $(".hero-title").lettering();
-    $('.carousel').owlCarousel({
-        loop:false,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:1,
-                nav:false
-            },
-            1000:{
-                items:1,
-                nav:true,
-                loop:false
             }
-        }
-    });
-
-        $('.arrow span.next').on('touchstart click',function() {
-            var cls = $(this).closest("section").next().offset().top;
-            $('html, body').animate({scrollTop: cls}, 'slow');
-            return false;
         });
 
-
-});
-
-
-
-
+        if($( window ).width() < 720){
+            $('.card-box.owl-carousel').owlCarousel({
+                stagePadding: 30,
+                loop:false,
+                margin:5,
+                nav:true,
+                mergeFit:true,
+                items:2
+            });
+            $('.section_5.owl-carousel').owlCarousel({
+                loop:false,
+                margin:10,
+                merge:true,
+                navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+                responsiveClass:true,
+                responsive:{
+                    0:{
+                        items:1,
+                        nav:true
+                    },
+                    600:{
+                        items:1,
+                        nav:false
+                    },
+                    1000:{
+                        items:1,
+                        nav:true,
+                        loop:false
+                    }
+                }
+            });
+            $('.section_3.owl-carousel').owlCarousel({
+                loop:false,
+                margin:10,
+                merge:true,
+                navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+                responsiveClass:true,
+                responsive:{
+                    0:{
+                        items:1,
+                        nav:true
+                    },
+                    600:{
+                        items:1,
+                        nav:false
+                    },
+                    1000:{
+                        items:1,
+                        nav:true,
+                        loop:false
+                    }
+                }
+            });
+        }
+    });
